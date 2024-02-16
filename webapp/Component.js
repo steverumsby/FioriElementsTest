e1c37422a495123a2ec81b9e6926c601e1d35bab
Steve Rumsby <steve-sap@rumsby.org.uk>
2024-02-16 16:01:15 +0000

    Just initializing

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("zsjrui5test.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
